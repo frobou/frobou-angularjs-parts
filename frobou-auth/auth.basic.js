ee08f6a1479682563e77a760acc5394f71704508
Fabio Pimenta <fabio@ispti.com.br>
2016-09-26 15:48:44 -0300

    um

--- frobou-auth/auth.basic.js
+++ frobou-auth/auth.basic.js
@@ -1,51 +1,58 @@
-angular.module('frobou.auth')
-    .service('AuthBasicService', ['$http', 'md5', '$base64',
-        function ($http, md5, $base64) {
+(function (angular) {
+    'use strict';
+    angular.module('frobou.auth', []);
+})();
+(function(angular) {
+    'use strict';
+    angular.module('frobou.auth')
+        .service('AuthBasicService', ['$http', 'md5', '$base64',
+            function ($http, md5, $base64) {
 
-            var verifyConfig = function (config) {
-                if (config.username === undefined || config.password === undefined || config.url === undefined){
-                    return false;
-                }
-                if (config.username === '' || config.password === '' || config.url === ''){
-                    return false;
-                }
-                if (config.hash === undefined){
-                    config.hash = false;
-                }
-                if (config.method === undefined){
-                    config.method = 'GET';
-                }
-                if (config.headers === undefined){
-                    config.headers = {};
-                }
-                return true;
-            };
+                var verifyConfig = function (config) {
+                    if (config.username === undefined || config.password === undefined || config.url === undefined) {
+                        return false;
+                    }
+                    if (config.username === '' || config.password === '' || config.url === '') {
+                        return false;
+                    }
+                    if (config.hash === undefined) {
+                        config.hash = false;
+                    }
+                    if (config.method === undefined) {
+                        config.method = 'GET';
+                    }
+                    if (config.headers === undefined) {
+                        config.headers = {};
+                    }
+                    return true;
+                };
 
-            var createHeaders = function (config) {
-                var headers = {Authorization: 'Basic ' + $base64.encode(config.username + ':' + config.password)};
-                angular.forEach(values, function (value, key) {
-                    headers.key = value;
-                });
-                return headers;
-            };
+                var createHeaders = function (config) {
+                    var headers = {Authorization: 'Basic ' + $base64.encode(config.username + ':' + config.password)};
+                    angular.forEach(values, function (value, key) {
+                        headers.key = value;
+                    });
+                    return headers;
+                };
 
-            this.login = function (config) {
-                /* valida se o objeto de configuracao esta correto */
-                if (!verifyConfig(config)) {
-                    return false;
-                }
-                if (config.hash === true){
-                    config.password = md5.createHash(config.password);
-                }
-                return $http({
-                    method: config.method.toUpperCase(),
-                    url: config.url,
-                    headers: createHeaders(config)
-                }).then(function (response) {
-                    return angular.fromJson(response.data);
-                }, function (response) {
-                    return angular.fromJson(response.data);
-                });
-            };
-        }
-    ]);
+                this.login = function (config) {
+                    /* valida se o objeto de configuracao esta correto */
+                    if (!verifyConfig(config)) {
+                        return false;
+                    }
+                    if (config.hash === true) {
+                        config.password = md5.createHash(config.password);
+                    }
+                    return $http({
+                        method: config.method.toUpperCase(),
+                        url: config.url,
+                        headers: createHeaders(config)
+                    }).then(function (response) {
+                        return angular.fromJson(response.data);
+                    }, function (response) {
+                        return angular.fromJson(response.data);
+                    });
+                };
+            }
+        ]);
+})(angular);
